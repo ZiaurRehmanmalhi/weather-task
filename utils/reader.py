fa571915083cf7b01c16c7ca935e766ac53bbd62
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-03-20 11:16:12 +0500

    task-5

--- utils/reader.py
+++ utils/reader.py
@@ -5,5 +5,18 @@ def read_file():
 
         for row in file_contant.split("\n")[1:-1]:
             file_values.append(row)
-    return file_values
+            # print(row)
+        return file_values
+
+def read_file_2():
+    file_values = []
+    with open("/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f2.csv") as file_read:
+        file_contant = file_read.read()
+
+        for row in file_contant.split("\n")[1:-1]:
+            file_values.append(row)
+            # print(row)
+        return file_values
+
+
 
