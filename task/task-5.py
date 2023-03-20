4c322d812de5e411a5b76de80fa32f73eb9736d7
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-03-20 19:40:33 +0500

    changes

--- task/task-5.py
+++ task/task-5.py
@@ -1,7 +1,9 @@
-from utils.reader import read_file_2
-list_data = read_file_2()
+from utils.reader import read_file
+path_1 = "/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f1.csv"
+path_2 = "/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f2.csv"
+file_1, file_2 = read_file(path_1, path_2)
 
-for data in list_data:
+for data in file_2:
     date = data.split(",")[1]
     event = data.split(",")[-2]
 
