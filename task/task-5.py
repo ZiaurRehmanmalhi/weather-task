627aa2654d4e39e2a20d81fc6d521ec41cea0c8a
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-03-20 12:48:32 +0500

    changes

--- task/task-5.py
+++ task/task-5.py
@@ -5,6 +5,5 @@ for data in list_data:
     date = data.split(",")[1]
     event = data.split(",")[-2]
 
-    if event == "Rain" or event == "Snow" or event == "Rain-Snow":
-        print(f"{date}  ko  {event}")
-        
+    if event in ["Rain", "Snow", "Rain-Snow"]:
+        print(f"{date}  in  {event}")
