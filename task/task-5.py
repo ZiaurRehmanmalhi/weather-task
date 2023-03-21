ece09346f98c5d45c389f979962d77d20916660b
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-03-21 16:13:59 +0500

    name change

--- task/task-5.py
+++ task/task-5.py
@@ -1,9 +1,9 @@
 from utils.reader import read_file
-path = read_file("/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f2.csv")
+file_read_csv = read_file("/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f2.csv")
 
-for data in path:
-    date = data.split(",")[1]
-    event = data.split(",")[-2]
+for all_columns in file_read_csv:
+    date = all_columns.split(",")[1]
+    event = all_columns.split(",")[-2]
 
     if event in ["Rain", "Snow", "Rain-Snow"]:
         print(f"{date}  in  {event}")
--- utils/reader.py
+++ utils/reader.py
@@ -2,8 +2,8 @@ def read_file(path):
     file_values = []
 
     with open(path) as file_read:
-        file_contant = file_read.read()
-        for row in file_contant.split("\n")[1:-1]:
+        file_content = file_read.read()
+        for row in file_content.split("\n")[1:-1]:
             file_values.append(row)
 
     return file_values
