9694291bbd9e3aae0ebcf15cbe7058215a435162
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-03-21 13:20:30 +0500

    change

--- task/task-5.py
+++ task/task-5.py
@@ -1,9 +1,7 @@
 from utils.reader import read_file
-path_1 = "/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f1.csv"
-path_2 = "/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f2.csv"
-file_1, file_2 = read_file(path_1, path_2)
+path = read_file("/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f2.csv")
 
-for data in file_2:
+for data in path:
     date = data.split(",")[1]
     event = data.split(",")[-2]
 
--- utils/reader.py
+++ utils/reader.py
@@ -1,16 +1,9 @@
-def read_file(path_1, path_2):
-    file_values1 = []
-    file_values2 = []
-    with open(path_1) as file_read_1:
-        file_contant = file_read_1.read()
-        for row in file_contant.split("\n")[1:-1]:
-            file_values1.append(row)
-    with open("/home/zia/ISDP/Hammad sab/Python/wether_Mar/weather task 2 in csv file/files/f2.csv") as file_read:
+def read_file(path):
+    file_values = []
+
+    with open(path) as file_read:
         file_contant = file_read.read()
         for row in file_contant.split("\n")[1:-1]:
-            file_values2.append(row)
-    return file_values1, file_values2
+            file_values.append(row)
 
-
-
-
+    return file_values
